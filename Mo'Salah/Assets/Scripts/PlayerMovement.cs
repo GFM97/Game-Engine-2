0ded2ef30b77d6ffcb17d00b4a755c9325bf025a
GFM97 <33311109+GFM97@users.noreply.github.com>
2018-05-28 14:58:34 +0200

    - powerups
    - more power ups including increase and decrease speed, resizing object.
    - fake power ups.
    - respawn.
    -

--- Mo'Salah/Assets/Scripts/PlayerMovement.cs
+++ Mo'Salah/Assets/Scripts/PlayerMovement.cs
@@ -7,45 +7,80 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float forwardForce = 2000f;
 	public float sidewaysForce = 500f;
-	public float upwardForce = 200f;
+	public float timeLeft;
 	public GameManager manager;
+	public int counter = 0;
 
 	public KeyCode keyLeft;
 	public KeyCode keyRight;
 	public KeyCode keyUp;
+	public KeyCode keyDown;
 
 	// We marked this as "Fixed"Update because we 
 	// are using it to mess with physics.
 	void FixedUpdate ()
 	{
 		// Add a forward force
-		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+		rb.AddForce (0, 0, forwardForce * Time.deltaTime);
 
 		// Add a force to the right
 		// If the player is pressing the "D" key
-		if(Input.GetKey(keyRight))
-		{
+		if (Input.GetKey (keyRight)) {
 			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
 		// Add a force to the left
 		// If the player is pressing the "A" key
-		if(Input.GetKey(keyLeft))
-		{
+		if (Input.GetKey (keyLeft)) {
 			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
-		if(Input.GetKey(keyUp))
+		if (Input.GetKey (keyUp)) 
 		{
-			rb.AddForce (0,upwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+			if (counter == 1) {
+				transform.localScale += new Vector3 (Random.Range(-0.9f, 0.9f), Random.Range(-0.9f, 0.9f), Random.Range(-0.9f, 0.9f));
+				counter = 0;
+			}
+
+			if (counter == 2) {
+
+				rb.AddForce (0, 0, forwardForce * Time.deltaTime * Random.Range(-200f, 200f));
+				counter = 0;
+			}
+		}
+
+		if (Input.GetKey (keyDown)) 
+		{
+			if (counter == 1) {
+				transform.localScale += new Vector3 (Random.Range(-0.9f, 0.9f), Random.Range(-0.9f, 0.9f), Random.Range(-0.9f, 0.9f));
+				counter = 0;
+			}
+
+			if (counter == 2) {
+
+				rb.AddForce (0, 0, forwardForce * Time.deltaTime * Random.Range(-200f, 200f));
+				counter = 0;
+			}
+		}
+	}
+
+
+	public void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "PowerUp" && counter == 0)
+		{
+
+			counter = Random.Range(1, 3);
 		}
 	}
 
 	void Update ()
 	{
-		if (rb.position.y < -1f) {
+		if (rb.position.y < -1f) 
+		{
 			manager.EndGame ();
 
 		}
 	}
 }
+	
